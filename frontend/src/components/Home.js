5b1478cb3631ae839673b4669593bc11c4ee0d58
Nuwani Seram <113815488+NuwaniSeram@users.noreply.github.com>
2024-12-03 14:22:05 +0530

    Footer and Header

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import '../css/Home.css'
 
-const Home = () => (
-    <div className='home'>
-        Welcome
-    </div>
-);
-
-export default Home;
+const Home = () => {
+    return (
+      <div className="home">
+        <div className="banner">
+          <div className="banner-content">
+            <h1>Your Vision, Our Construction</h1>
+            <p>
+              We bring your ideas to life with expert craftsmanship and innovative solutions. Let's build something amazing together.
+            </p>
+            <button className="banner-btn">Get Started</button>
+          </div>
+        </div>
+        {/* Other homepage content can go here */}
+      </div>
+    );
+  };
+  
+  export default Home;
