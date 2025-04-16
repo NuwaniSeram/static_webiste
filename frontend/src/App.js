e21f964f4064f690064cfbbee441312490d6d4db
NuwaniSeram <nuwaniseram@gmail.com>
2025-04-16 16:24:49 +0530

    adding routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,11 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from './components/Home';
+import Projects from "./components/Projects";
+import ProjectDetails from "./components/ProjectDetails";
+import AboutUs from "./components/AboutUs";
+import ContactUs from "./components/ContactUs";
+
 import './App.css'; // Add CSS for layout adjustments
 
 function App() {
@@ -13,6 +18,10 @@ function App() {
         <div className="main-content">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/projects/:id" element={<ProjectDetails />} />
+            <Route path="/about-us" element={<AboutUs />} />
+            <Route path="/contact-us" element={<ContactUs/>} />
           </Routes>
         </div>
         <Footer />
