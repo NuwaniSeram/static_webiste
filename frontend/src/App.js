24733f6c7972217e06e5275d6746e05fcb8cbfbe
NuwaniSeram <nuwaniseram@gmail.com>
2025-04-16 15:46:15 +0530

    projects

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,10 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from './components/Home';
-import './App.css'; // Add CSS for layout adjustments
+import Projects from "./components/Projects";
+import ProjectDetails from "./components/ProjectDetails";
+
+import './App.css'; 
 
 function App() {
   return (
@@ -13,6 +16,8 @@ function App() {
         <div className="main-content">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/projects/:id" element={<ProjectDetails />} /> 
           </Routes>
         </div>
         <Footer />
