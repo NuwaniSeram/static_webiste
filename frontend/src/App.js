74e8849f9d29b4be012eacdb64016af85b9b58bd
NuwaniSeram <nuwaniseram@gmail.com>
2025-07-02 23:07:42 +0530

    News and Media

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,8 +7,9 @@ import Projects from "./components/Projects";
 import ProjectDetails from "./components/ProjectDetails";
 import AboutUs from "./components/AboutUs";
 import ContactUs from "./components/ContactUs";
-
-import './App.css'; // Add CSS for layout adjustments
+import NewsMedia from "./components/NewsMedia";
+import NewsArticleDetail from "./components/NewsArticleDetail"; 
+import './App.css';
 
 function App() {
   return (
@@ -22,6 +23,8 @@ function App() {
             <Route path="/projects/:id" element={<ProjectDetails />} />
             <Route path="/about-us" element={<AboutUs />} />
             <Route path="/contact-us" element={<ContactUs/>} />
+            <Route path="/news-media" element={<NewsMedia/>} />
+            <Route path="/news-media/article/:id" element={<NewsArticleDetail/>} /> {/* Add this route */}
           </Routes>
         </div>
         <Footer />
